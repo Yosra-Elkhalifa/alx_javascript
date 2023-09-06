298c993c2ef52843b4c60d984fc6c85ffb28f1d7
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-09-06 23:57:55 +0200

    add me mybe

---
+++ javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,10 @@
+function addMeMaybe (number, theFunction)
+{
+    number += 1
+    theFunction(number)
+    
+}
+
+// has to be called and console.log wiil be in the inner function
+
+module.exports.addMeMaybe = addMeMaybe
