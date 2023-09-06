7e5c93fca476850afa92a7b1690ee5b5d95341ca
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-09-07 00:23:45 +0200

    object_fct

---
+++ javascript-warm_up/103-object_fct.js
@@ -0,0 +1,20 @@
+const myObject = 
+{
+    type: 'object',
+    value: 12,
+}
+
+console.log(myObject)
+
+myObject.incr = function incr ()
+{
+    myObject.value = myObject.value + 1
+    return myObject.value 
+}
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
