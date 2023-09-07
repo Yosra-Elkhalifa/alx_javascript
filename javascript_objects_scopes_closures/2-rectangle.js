60690e167cce1edbb6c186304c4d4c21b7909717
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-09-07 14:20:21 +0200

    updating class rectangle

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,16 @@
+class Rectangle
+{
+    constructor (w, h)
+    {
+        this.width = w
+        this.height = h
+        if (w <= 0 || h <= 0)
+        {
+            return Rectangle
+        }
+        
+    }
+
+};
+
+module.exports = Rectangle;
