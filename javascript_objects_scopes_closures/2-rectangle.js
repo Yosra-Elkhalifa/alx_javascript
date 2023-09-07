3c5b87f43bf5c003946e9d95d9066426d56469bf
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-09-07 14:33:58 +0200

    returning empty object

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,13 +3,12 @@ class Rectangle
 {
     constructor (w, h)
     {
-        this.width = w
-        this.height = h
         if (w <= 0 || h <= 0)
         {
-            return constructor()
+            return {}
         }
-        
+        this.width = w
+        this.height = h
     }
 
 };
