d97abb4397b8a1112a900ce0c13fa5f85b428b2b
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-09-07 16:23:13 +0200

    adding rotate and double functions

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+class Rectangle
+{
+    constructor (w, h)
+    {
+        if (w && h > 0)
+        {
+            this.width = w
+            this.height = h   
+            return this.width, this.width
+        }
+    }
+    print()
+        {
+            for (let i=0; i<this.height; i++)
+            {
+                console.log('X'.repeat(this.width))
+            }
+        }
+    rotate()
+    {
+        for (let i=0; i<this.width; i++)
+            {
+                console.log('X'.repeat(this.height))
+            }
+    }
+    double()
+    {
+        let double_h = this.height * 2
+        let double_w = this.width * 3
+        for (let i=0; i< double_h; i++)
+            {
+                console.log('X'.repeat(double_w))
+            }
+    }
+};
+
+module.exports = Rectangle;
