aecc55643ce6904ffeb38b7e8831bed6fec03926
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-09-07 15:32:42 +0200

    Task 2 commit

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,11 +3,10 @@ class Rectangle
 {
     constructor (w, h)
     {
-        this.width = w
-        this.height = h
         if (this.width <= 0 || this.height <= 0)
         {
-            
+            this.width = w
+            this.height = h   
             return this.width, this.width
         }
         
