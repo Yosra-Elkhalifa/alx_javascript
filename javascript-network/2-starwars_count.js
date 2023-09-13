44dd5bfa97083b5679e159f193138088023b8174
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-09-13 21:54:23 +0200

    Starwars count

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request.get(url, function(error, response, body){
+    const films = JSON.parse(body);
+    const character = films.results.filter((film)=>
+        film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+        
+    );
+
+    const count = character.length;
+    console.log(count);
+});
