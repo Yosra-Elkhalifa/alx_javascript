44200bd4814bf23b8d5214777f72e5d9763c674f
Yosra Elkhalifa <yosra.osman93@gmail.com>
2023-09-13 22:43:53 +0200

    starwars count using wildcards

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -7,8 +7,7 @@ const url = process.argv[2];
 request.get(url, function(error, response, body){
     const films = JSON.parse(body);
     const character = films.results.filter((film)=>
-        film.characters.includes(`https://swapi-api.alx-tools.com/api/people/18/`|| `http://swapi.co/api/people/18/`)
-        
+        film.characters.some((characterUrl) => characterUrl.endsWith('/18/'))
     );
 
     const count = character.length;
